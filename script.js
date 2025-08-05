fee5c7cc62b0e0a7ef420aef444d73f955f6e11b
musa-bojang <musabojang413@gmail.com>
2025-08-05 23:17:07 +0100

    Build a Dynamic To-Do List Application

---
+++ script.js
@@ -0,0 +1,33 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
+   const addButton = document.getElementById('add-task-btn');
+   const taskInput = document.getElementById('task-input');
+   const taskList = document.getElementById('task-list');
+
+   function addTask() {
+       const taskText = taskInput.value.trim();
+       if(taskText){
+              const taskItem = document.createElement('li');
+              taskItem.textContent = taskText;
+    
+              const removeButton = document.createElement('button');
+              removeButton.textContent = 'Remove';
+              removeButton.classList.add('remove-btn');
+    
+              removeButton.addEventListener('click', function() {
+                taskList.removeChild(taskItem);
+              });
+    
+              taskItem.appendChild(removeButton);
+              taskList.appendChild(taskItem);
+    
+              taskInput.value = ''; // Clear the input field
+      
+       }else {
+           alert('Please enter a task.');
+       }
+   }
+
+    addButton.addEventListener('click', addTask);
+});
